2152fb6803e54ecf1b9181d5ce24c05ae3e6698c
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-27 15:22:57 +0900

    add Project 10, part 1

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,9 +9,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
+        }
+    }
+    
+//    func loadData() {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        let request = URLRequest(url: url)
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    // we have good data – go back to the main thread
+//                    DispatchQueue.main.async {
+//                        // update our UI
+//                        self.results = decodedResponse.results
+//                    }
+//
+//                    // everything is good, so we can exit
+//                    return
+//                }
+//            }
+//
+//            // if we're still here it means there was a problem
+//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//        }.resume()
+//    }
+}
+
+class User: ObservableObject, Codable {
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Paul Hudson"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView_Previews: PreviewProvider {
